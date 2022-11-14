b6a100d030c0aa087f05c38f3c1cc77a335600d6
Adrian Høyheim Vigdal <adhvi@fylkesmannen.no>
2022-11-14 15:16:12 +0100

    added azure db and identity pages

---
+++ vm22MVC/Areas/Identity/Data/vm22MVCContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace vm22MVC.Data;
+
+public class vm22MVCContext : IdentityDbContext<IdentityUser>
+{
+    public vm22MVCContext(DbContextOptions<vm22MVCContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
--- vm22MVC/Program.cs
+++ vm22MVC/Program.cs
@@ -1,4 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using vm22MVC.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("vm22MVCContextConnection") ?? throw new InvalidOperationException("Connection string 'vm22MVCContextConnection' not found.");
+
+builder.Services.AddDbContext<vm22MVCContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<vm22MVCContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -17,6 +27,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();;
 
 app.UseAuthorization();
 
