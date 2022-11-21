a9dd1521882435461320ff649fc3ad902af24efd
Adrian Høyheim Vigdal <adhvi@fylkesmannen.no>
2022-11-21 15:29:31 +0100

    he

--- vm22MVC/Controllers/LeaderboardController.cs
+++ vm22MVC/Controllers/LeaderboardController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Diagnostics;
+using Newtonsoft.Json.Linq;
+using vm22MVC.Models;
+using getAPIstuff.Models;
 
 namespace vm22MVC.Controllers
 {
@@ -12,19 +15,28 @@ namespace vm22MVC.Controllers
             DirectoryInfo di = new DirectoryInfo(filePath);
             FileInfo[] fileInfos = di.GetFiles();
             Debug.WriteLine("before foreach?");
+            var listModel = new List<TippeModel>();
+
             foreach (var file in fileInfos)
             {
                 Debug.WriteLine(file.Name);
                 //System.Text.Encoding.Default gives me ÆØÅ
                 using StreamReader r = new StreamReader(file.FullName, System.Text.Encoding.Default);
                 string json = r.ReadToEnd();
-                string jsonString = System.Text.Json.JsonSerializer.Serialize(json);
-                Debug.WriteLine(jsonString);
-                dynamic array = JsonConvert.DeserializeObject(jsonString);
-                Debug.WriteLine(array[1].kampModels.nifsKampId);
-                foreach (var item in array)
+                string jsonSerializedString = System.Text.Json.JsonSerializer.Serialize(json);
+                Debug.WriteLine(jsonSerializedString);
+                var jToken = JToken.Parse(jsonSerializedString);
+
+                foreach (var item in jToken)
                 {
-                    Debug.WriteLine($"{item.kampModels.nifsKampId}. Svar: {item.TippeModels.HjemmeLag}-{item.TippeModels.Bortelag}:{item.TippeModels.Answer}");
+                    var nifsKampId = (int)item.SelectToken("kampModels.nifsKampId");
+                    Debug.WriteLine((int)item.SelectToken("kampModels.nifsKampId"));
+                    var model = new TippeModel()
+                    {
+                        NifsKampId = (string)item.SelectToken("kampModels.nifsKampId")
+                    };
+                    listModel.Add(model);
+                    //Debug.WriteLine(item.kampModels.nifsKampId);
                 }
             }
             return View();
