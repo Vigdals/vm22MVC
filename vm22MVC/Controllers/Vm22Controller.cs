eb684f7ce0baee986265ab45041e07fb6d0bf9c7
Asmund B <asbre@fylkesmannen.no>
2022-11-23 12:54:10 +0100

    Changed VM22
    #Added a way to get groups that does not use alphabetical identifier in the group name

--- vm22MVC/Controllers/Vm22Controller.cs
+++ vm22MVC/Controllers/Vm22Controller.cs
@@ -4,6 +4,7 @@ using getAPIstuff.Models;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
 using System.Diagnostics;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
 using vm22MVC.Models;
 using vm22MVC.Service;
 
@@ -44,11 +45,40 @@ namespace vm22MVC.Controllers
             }
 
             //Using Linq here with input fra drop down list in the index.cshtml:
-            if (groupName == null)
+            if (string.IsNullOrWhiteSpace(groupName))
             {
                 return View(listModel.First());
             }
-            return View(listModel.First(x => x.groupName == groupName));
+
+            //If the list model contains a group name that has a alphabetical identifier it returns said model with that group name.
+            //If not it will attempt to find the corresponding group with a numerical identifier.
+            if (listModel.Any(x => x.groupName.Contains(groupName))) return View(listModel.First(x => x.groupName == groupName));
+            {
+                try
+                {
+                    //Switch that returns the corresponding group name with the group name numerical identifier.
+                    //This is done since some values int he api does not use group as a alphabetical identifier but rather as a numerical.
+                    return groupName.ToLower() switch
+                    {
+                        "gruppe a" => View(listModel.First(x => x.groupName == "Gruppe 1")),
+                        "gruppe b" => View(listModel.First(x => x.groupName == "Gruppe 2")),
+                        "gruppe c" => View(listModel.First(x => x.groupName == "Gruppe 3")),
+                        "gruppe d" => View(listModel.First(x => x.groupName == "Gruppe 4")),
+                        "gruppe e" => View(listModel.First(x => x.groupName == "Gruppe 5")),
+                        "gruppe f" => View(listModel.First(x => x.groupName == "Gruppe 6")),
+                        "gruppe g" => View(listModel.First(x => x.groupName == "Gruppe 7")),
+                        "gruppe h" => View(listModel.First(x => x.groupName == "Gruppe 8")),
+                        _ => View(listModel.First())
+                    };
+                }
+                catch
+                {
+                    //If the switch fails to return a view due to an exception from not finding a numerical group in the incoming
+                    //api request. It returns an empty TournamentModel and sends a message to the user with Viewbag
+                    ViewBag.Message = $"Fant ikkje {groupName}!";
+                    return View(new TournamentModel() { kampModels = new List<kampModel>() });
+                }
+            }
         }
     }
 }
