81437747088dcddb76be5e08856f9527a039f8c9
Adrian Høyheim Vigdal <adhvi@fylkesmannen.no>
2022-11-23 01:07:12 +0100

    tippeoversikt added

--- vm22MVC/Controllers/LeaderboardController.cs
+++ vm22MVC/Controllers/LeaderboardController.cs
@@ -3,38 +3,47 @@ using Newtonsoft.Json.Linq;
 using System.Diagnostics;
 using System.IdentityModel.Tokens.Jwt;
 using System.Text.Json;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using vm22MVC.Models;
 
 namespace vm22MVC.Controllers
 {
     public class LeaderboardController : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string userName)
         {
+            ViewBag.UserName = userName;
             //filsti til alle jsons i virtuell mappe i Azure static webapp
+            if (string.IsNullOrWhiteSpace(userName)) return View(new List<TournamentModel>());
+            var enivornment = Environment.UserDomainName;
             var filePath = "c:\\home\\json\\correctJsonFolder\\";
+            Debug.WriteLine(enivornment);
             DirectoryInfo di = new DirectoryInfo(filePath);
             FileInfo[] fileInfos = di.GetFiles();
-            Debug.WriteLine("before foreach?");
-
+            var prefixedFilename = $"Odds konk_{userName.ToUpper()}.json";
             var tournamentModelList = new List<TournamentModel>();
 
-            foreach (var file in fileInfos)
+            foreach (var file in fileInfos.Where(f => f.Name == prefixedFilename))
             {
-                Debug.WriteLine(file.Name);
+                Debug.WriteLine($"----{file.Name}----");
                 //System.Text.Encoding.Default gives me ÆØÅ - good
                 using StreamReader r = new StreamReader(file.FullName, System.Text.Encoding.Default);
                 string json = r.ReadToEnd();
 
                 if (string.IsNullOrWhiteSpace(json)) return View("Error");
-                Debug.WriteLine(json);
+                //Debug.WriteLine(json);
 
                 var deserialized = JObject.Parse(json);
-                Debug.WriteLine(deserialized);
+                //Debug.WriteLine(deserialized);
 
+                //Under her er Asmund kode som Adrian ikkje forstår heilt. Og den fungerer ikkje
 
+                foreach (var item in deserialized)
+                {
+                    Debug.WriteLine(item.Key);
+                    Debug.WriteLine(item.Value);
+                }
 
-                //Under her er Asmund kode som Adrian ikkje forstår heilt. Og den fungerer ikkje heilt heller:
                 foreach (var keyValuePair in deserialized)
                 {
                     var listTippeModels = GetTippeModels(keyValuePair);
@@ -46,28 +55,25 @@ namespace vm22MVC.Controllers
                     });
                 }
             }
-
             return View(tournamentModelList);
         }
 
         List<TippeModel> GetTippeModels(KeyValuePair<string, JToken?> keyValuePair)
         {
             var tippeModelList = new List<TippeModel>();
-
-            if (keyValuePair.Value == null) throw new Exception("No tippe models found");
-
-            var keyValueList = keyValuePair.Value.ToList();
-
-            for (var index = 0; index < keyValueList.Count; index++)
+            if (keyValuePair.Value != null)
             {
-                tippeModelList.Add(JsonSerializer.Deserialize<TippeModel>(keyValuePair.Value[index]?.ToString() ?? string.Empty) ?? new TippeModel());
-                if (keyValuePair.Value.Count() >= index && tippeModelList.Count != 0)
-                {
-                    return tippeModelList;
-                }
+                var keyValueList = keyValuePair.Value.ToList();
             }
+            else return tippeModelList;
 
-            throw new Exception("No tippe models found");
+            foreach (var jToken in keyValuePair.Value)
+            {
+                var deserializedTippeModel = JsonSerializer.Deserialize<TippeModel>(jToken.ToString());
+                if (deserializedTippeModel != null) tippeModelList.Add(deserializedTippeModel);
+
+            }
+            return tippeModelList;
         }
     }
 }
