4b06d3e8ea12b27c287738a6691a5d1ded43435f
Adrian Høyheim Vigdal <adhvi@fylkesmannen.no>
2022-12-13 16:28:06 +0100

    revmoed some shit. Added proper date parsing in tipping. Found a bug that makes you login without any user name :o

--- vm22MVC/Program.cs
+++ vm22MVC/Program.cs
@@ -1,15 +1,9 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using vm22MVC.Data;
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("vm22MVCContextConnection") ?? throw new InvalidOperationException("Connection string 'vm22MVCContextConnection' not found.");
 
-builder.Services.AddDbContext<vm22MVCContext>(options =>
-    options.UseSqlServer(connectionString));
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<vm22MVCContext>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
